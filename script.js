d543cf5b98ffbc46aef3b9aa7411de2a5d389784
Ajay6786239 <ap6786239@gmail.com>
2024-11-28 21:19:20 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,679 @@
+var uuu=document.getElementById("uuu")
+
+var cat1=document.querySelector(".cat1")
+var cat1image=document.querySelector(".cat1image")
+var x=document.getElementById("xa")
+
+
+uuu.addEventListener("click",function(){
+    cat1image.style.display="flex"
+    xa.style.display="block"
+    event.preventDefault()
+})
+
+xa.addEventListener("click",function(){
+    event.preventDefault()
+    cat1image.style.display="none"
+    xa.style.display="none"
+})
+var mmm=document.getElementById("mmm")
+var cat2=document.querySelector(".cat2")
+var cat2image=document.querySelector(".cat2image")
+var xb=document.getElementById("xb")
+mmm.addEventListener("click" ,function(){
+    cat2image.style.display="flex"
+    xb.style.display="block"
+    event.preventDefault()
+})
+xb.addEventListener("click",function(){
+    event.preventDefault()
+    cat2image.style.display="none"
+    xb.style.display="none"
+})
+var mmab=document.querySelector("#mmab")
+var cat3=document.querySelector(".cat3")
+var cat3image=document.querySelector(".cat3image")
+var xc=document.getElementById("xc")
+
+mmab.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat3image.style.display = "flex";
+    xc.style.display = "block";
+});
+
+
+xc.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat3image.style.display = "none";
+    xc.style.display = "none";
+});
+
+var mmb=document.querySelector("#mmb")
+var cat4=document.querySelector(".cat4")
+var cat4image=document.querySelector(".cat4image")
+var xc=document.getElementById("xc")
+
+mmb.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat4image.style.display = "flex";
+    xd.style.display = "block";
+});
+
+
+xd.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat4image.style.display = "none";
+    xd.style.display = "none";
+});
+
+var mmc=document.querySelector("#mmc")
+var cat5=document.querySelector(".cat5")
+var cat5image=document.querySelector(".cat5image")
+var xc=document.getElementById("xc")
+
+mmc.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat5image.style.display = "flex";
+    xe.style.display = "block";
+});
+
+
+xe.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat5image.style.display = "none";
+    xe.style.display = "none";
+});
+
+var mmd=document.querySelector("#mmd")
+var cat6=document.querySelector(".cat6")
+var cat6image=document.querySelector(".cat6image")
+var xf=document.getElementById("xf")
+
+mmd.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat6image.style.display = "flex";
+    xf.style.display = "block";
+});
+
+
+xf.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat6image.style.display = "none";
+    xf.style.display = "none";
+});
+
+var mme=document.querySelector("#mme")
+var cat7=document.querySelector(".cat7")
+var cat7image=document.querySelector(".cat7image")
+var xg=document.getElementById("xg")
+
+mme.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat7image.style.display = "flex";
+    xg.style.display = "block";
+});
+
+
+xg.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat7image.style.display = "none";
+    xg.style.display = "none";
+});
+
+var mmf=document.querySelector("#mmf")
+var cat8=document.querySelector(".cat8")
+var cat8image=document.querySelector(".cat8image")
+var xh=document.getElementById("xh")
+
+mmf.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat8image.style.display = "flex";
+    xh.style.display = "block";
+});
+
+
+xh.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat8image.style.display = "none";
+    xh.style.display = "none";
+});
+
+var mmg=document.querySelector("#mmg")
+var cat9=document.querySelector(".cat9")
+var cat9image=document.querySelector(".cat9image")
+var xi=document.getElementById("xi")
+
+mmg.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat9image.style.display = "flex";
+    xi.style.display = "block";
+});
+
+
+xi.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat9image.style.display = "none";
+    xi.style.display = "none";
+});
+
+var mmh=document.querySelector("#mmh")
+var cat10=document.querySelector(".cat10")
+var cat10image=document.querySelector(".cat10image")
+var xj=document.getElementById("xj")
+
+mmh.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat10image.style.display = "flex";
+    xj.style.display = "block";
+});
+
+
+xj.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat10image.style.display = "none";
+    xj.style.display = "none";
+});
+
+var mmi=document.querySelector("#mmi")
+var cat11=document.querySelector(".cat11")
+var cat11image=document.querySelector(".cat11image")
+var xk=document.getElementById("xk")
+
+mmi.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat11image.style.display = "flex";
+    xk.style.display = "block";
+});
+
+
+xk.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat11image.style.display = "none";
+    xk.style.display = "none";
+});
+var mmj=document.querySelector("#mmj")
+var cat12=document.querySelector(".cat12")
+var cat12image=document.querySelector(".cat12image")
+var xl=document.getElementById("xl")
+
+mmj.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat12image.style.display = "flex";
+    xl.style.display = "block";
+});
+
+
+xl.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat12image.style.display = "none";
+    xl.style.display = "none";
+});
+
+var mmk=document.querySelector("#mmk")
+var cat13=document.querySelector(".cat13")
+var cat13image=document.querySelector(".cat13image")
+var xm=document.getElementById("xm")
+
+mmk.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat13image.style.display = "flex";
+    xm.style.display = "block";
+});
+
+
+xm.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat13image.style.display = "none";
+    xm.style.display = "none";
+});
+
+var mml=document.querySelector("#mml")
+var cat14=document.querySelector(".cat14")
+var cat14image=document.querySelector(".cat14image")
+var xn=document.getElementById("xn")
+
+mml.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat14image.style.display = "flex";
+    xn.style.display = "block";
+});
+
+
+xn.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat14image.style.display = "none";
+    xn.style.display = "none";
+});
+
+var mmzz=document.querySelector("#mmzz")
+var cat15=document.querySelector(".cat15")
+var cat15image=document.querySelector(".cat15image")
+var xo=document.getElementById("xo")
+
+mmzz.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat15image.style.display = "flex";
+    xo.style.display = "block";
+});
+
+
+xo.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat15image.style.display = "none";
+    xo.style.display = "none";
+});
+
+var mmn=document.querySelector("#mmn")
+var cat16=document.querySelector(".cat16")
+var cat16image=document.querySelector(".cat16image")
+var xp=document.getElementById("xp")
+
+mmn.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat16image.style.display = "flex";
+    xp.style.display = "block";
+});
+
+
+xp.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat16image.style.display = "none";
+    xp.style.display = "none";
+});
+
+var mmo=document.querySelector("#mmo")
+var cat17=document.querySelector(".cat17")
+var cat17image=document.querySelector(".cat17image")
+var xq=document.getElementById("xq")
+
+mmo.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat17image.style.display = "flex";
+    xq.style.display = "block";
+});
+
+
+xq.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat17image.style.display = "none";
+    xq.style.display = "none";
+});
+var mmp=document.querySelector("#mmp")
+var cat18=document.querySelector(".cat18")
+var cat18image=document.querySelector(".cat18image")
+var xr=document.getElementById("xr")
+
+mmp.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat18image.style.display = "flex";
+    xr.style.display = "block";
+});
+
+
+xr.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat18image.style.display = "none";
+    xr.style.display = "none";
+});
+var mmq=document.querySelector("#mmq")
+var cat19=document.querySelector(".cat19")
+var cat19image=document.querySelector(".cat19image")
+var xs=document.getElementById("xs")
+
+mmq.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat19image.style.display = "flex";
+    xs.style.display = "block";
+});
+
+
+xs.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat19image.style.display = "none";
+    xs.style.display = "none";
+});
+var mmr=document.querySelector("#mmr")
+var cat20=document.querySelector(".cat20")
+var cat20image=document.querySelector(".cat20image")
+var xt=document.getElementById("xt")
+
+mmr.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat20image.style.display = "flex";
+    xt.style.display = "block";
+});
+
+
+xt.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat20image.style.display = "none";
+    xt.style.display = "none";
+});
+var mms=document.querySelector("#mms")
+var cat21=document.querySelector(".cat21")
+var cat21image=document.querySelector(".cat21image")
+var xu=document.getElementById("xu")
+
+mms.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat21image.style.display = "flex";
+    xu.style.display = "block";
+});
+
+
+xu.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat21image.style.display = "none";
+    xu.style.display = "none";
+});
+
+var mmt=document.querySelector("#mmt")
+var cat22=document.querySelector(".cat22")
+var cat22image=document.querySelector(".cat22image")
+var xv=document.getElementById("xv")
+
+mmt.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat22image.style.display = "flex";
+    xv.style.display = "block";
+});
+
+
+xv.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat22image.style.display = "none";
+    xv.style.display = "none";
+});
+
+var mmu=document.querySelector("#mmu")
+var cat23=document.querySelector(".cat23")
+var cat23image=document.querySelector(".cat23image")
+var xw=document.getElementById("xw")
+
+mmu.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat23image.style.display = "flex";
+    xw.style.display = "block";
+});
+
+
+xw.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat23image.style.display = "none";
+    xw.style.display = "none";
+});
+var mmv=document.querySelector("#mmv")
+var cat24=document.querySelector(".cat24")
+var cat24image=document.querySelector(".cat24image")
+var xx=document.getElementById("xx")
+
+mmv.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat24image.style.display = "flex";
+    xx.style.display = "block";
+});
+
+
+xx.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat24image.style.display = "none";
+    xx.style.display = "none";
+});
+var mmw=document.querySelector("#mmw")
+var cat25=document.querySelector(".cat25")
+var cat25image=document.querySelector(".cat25image")
+var xy=document.getElementById("xy")
+
+mmw.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat25image.style.display = "flex";
+    xy.style.display = "block";
+});
+
+
+xy.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat25image.style.display = "none";
+    xy.style.display = "none";
+});
+var mmx=document.querySelector("#mmx")
+var cat26=document.querySelector(".cat26")
+var cat26image=document.querySelector(".cat26image")
+var xac=document.getElementById("xac")
+
+mmx.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat26image.style.display = "flex";
+    xac.style.display = "block";
+});
+
+
+xac.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat26image.style.display = "none";
+    xac.style.display = "none";
+});
+var mmy=document.querySelector("#mmy")
+var cat27=document.querySelector(".cat27")
+var cat27image=document.querySelector(".cat27image")
+var xaa=document.getElementById("xaa")
+
+mmy.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat27image.style.display = "flex";
+    xaa.style.display = "block";
+});
+
+
+xaa.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat27image.style.display = "none";
+    xaa.style.display = "none";
+});
+var mmz=document.querySelector("#mmz")
+var cat28=document.querySelector(".cat28")
+var cat28image=document.querySelector(".cat28image")
+var xbb=document.getElementById("xbb")
+
+mmz.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat28image.style.display = "flex";
+    xbb.style.display = "block";
+});
+
+
+xbb.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat28image.style.display = "none";
+    xbb.style.display = "none";
+});
+
+var mmaa=document.querySelector("#mmaa")
+var cat29=document.querySelector(".cat29")
+var cat29image=document.querySelector(".cat29image")
+var xcc=document.getElementById("xcc")
+
+mmaa.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat29image.style.display = "flex";
+    xcc.style.display = "block";
+});
+
+
+xcc.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat29image.style.display = "none";
+    xcc.style.display = "none";
+});
+
+var mmbb=document.querySelector("#mmbb")
+var cat30=document.querySelector(".cat30")
+var cat30image=document.querySelector(".cat30image")
+var xdd=document.getElementById("xdd")
+
+mmbb.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat30image.style.display = "flex";
+    xdd.style.display = "block";
+});
+
+
+xdd.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat30image.style.display = "none";
+    xdd.style.display = "none";
+});
+
+var mmcc=document.querySelector("#mmcc")
+var cat31=document.querySelector(".cat31")
+var cat31image=document.querySelector(".cat31image")
+var xee=document.getElementById("xee")
+
+mmcc.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat31image.style.display = "flex";
+    xee.style.display = "block";
+});
+
+
+xee.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat31image.style.display = "none";
+    xee.style.display = "none";
+});
+
+var mmdd=document.querySelector("#mmdd")
+var cat32=document.querySelector(".cat32")
+var cat32image=document.querySelector(".cat32image")
+var xff=document.getElementById("xff")
+
+mmdd.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat32image.style.display = "flex";
+    xff.style.display = "block";
+});
+
+
+xff.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat32image.style.display = "none";
+    xff.style.display = "none";
+});
+
+var mmee=document.querySelector("#mmee")
+var cat33=document.querySelector(".cat33")
+var cat33image=document.querySelector(".cat33image")
+var xgg=document.getElementById("xgg")
+
+mmee.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat33image.style.display = "flex";
+    xgg.style.display = "block";
+});
+
+
+xgg.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat33image.style.display = "none";
+    xgg.style.display = "none";
+});
+
+var mmff=document.querySelector("#mmff")
+var cat34=document.querySelector(".cat34")
+var cat34image=document.querySelector(".cat34image")
+var xhh=document.getElementById("xhh")
+
+mmff.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat34image.style.display = "flex";
+    xhh.style.display = "block";
+});
+
+
+xhh.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat34image.style.display = "none";
+    xhh.style.display = "none";
+});
+var mmkk=document.querySelector("#mmkk")
+var cat35=document.querySelector(".cat35")
+var cat35image=document.querySelector(".cat35image")
+var xii=document.getElementById("xii")
+
+mmkk.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat35image.style.display = "flex";
+    xii.style.display = "block";
+});
+
+
+xii.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    cat35image.style.display = "none";
+    xii.style.display = "none";
+});
+
+
+var s = document.querySelector(".sidebar");
+var threedot = document.querySelector(".threedot");
+function change(){
+     s.style.left="0%"
+}
+function m(){
+    s.style.left="-55%"
+}
+
+
+
+
+/*search.addEventListener("keyup",function(){
+    var enteredvalue=event.target.value.toUpperCase()
+
+    for(var count=0;count<productlist.length; count=count+1){
+        var productname=productlist[count].querySelector("a").textContent
+
+        if(productname.toUpperCase().indexOf(enteredvalue)<0){
+            productlist[count].style.display="none"
+        }
+        else{
+            productlist[count].style.display="block"
+        }
+    }
+})*/
+var search = document.querySelector("#search");
+var productcontainer = document.querySelector(".cat");
+var productlist = [];
+
+// Assuming your class names are cat1, cat2, ..., cat35
+for (var i = 1; i <= 35; i++) {
+    productlist[i] = document.querySelector(`.cat${i}`);
+}
+
+// Now you can use productlist[i] to access each product
+console.log(productlist);
+
+search.addEventListener("keyup", function(event) {
+    var enteredvalue = event.target.value.toUpperCase();
+
+    for (var count = 1; count < productlist.length; count++) {
+        var product = productlist[count];
+        
+        // Ensure product exists
+        if (product) {
+            var productname = product.querySelector("a") ? product.querySelector("a").textContent : '';
+
+            if (productname.toUpperCase().indexOf(enteredvalue) < 0) {
+                product.style.display = "none";
+            } else {
+                product.style.display = "block";
+            }
+        }
+    }
+});
+
+
+
+
+
+
+
---
+++ script2.js
@@ -0,0 +1,8 @@
+var s = document.querySelector(".sidebar");
+var threedot = document.querySelector(".threedot");
+function change(){
+     s.style.left="0%"
+}
+function m(){
+    s.style.left="-70%"
+}
